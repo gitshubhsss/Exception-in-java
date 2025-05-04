f8cf95efa0a1d46a41838ae1c08439a2db0a44c6
gitshubhsss <shubhamranjane16@gmail.com>
2025-05-04 11:56:59 +0530

    i can create my own exceptions as well

---
+++ Main.java
@@ -0,0 +1,26 @@
+class Shubham extends Exception {
+
+    public Shubham(String s) {
+        super(s);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 4;
+        int b = 5;
+
+        int res = a / b;
+
+        try {
+            if (a < b) {
+                throw new Shubham("a should be greater that or equal to b");
+            }
+        } catch (Exception e) {
+            System.out.println(e+"sdflaskdjflaskdfjalskdfjalsfjasdl;fkja'of");
+        }
+
+        System.out.println(res);
+    }
+}
